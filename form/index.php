80d66eea87bd9e7214dafcac745b90b32cdebab3
ryota-toyomura <miitoyo291@gmail.com>
2023-09-28 11:55:25 +0900

    ブランチ追加

--- form/index.php
+++ form/index.php
@@ -12,7 +12,7 @@ include('./assets/inc/form-setup.php');
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="keywords" content="">
     <meta name="description" content="">
-    <title>フォーム　テストテンプレート</title>
+    <title>テストテンプレート</title>
     <link rel="stylesheet" href="assets/css/style.css">
 </head>
 
